9ba0c28b35f0186afff25d0c1ef9b093054c833c
Aneesh Sharma <aneeshsharma0671@gmail.com>
2025-01-06 12:27:50 +0530

    tictactoeDesign

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,30 +4,57 @@ namespace ConsoleApp {
 	class Program {
 		static void Main(string[] args) {
 			TicTacToe game = new TicTacToe();
-			game.Initialize();
+
+			game.OnMoveMade += (sender, e) => {
+				// Print board
+				List<List<TicTacToe.PlayerToken>> board = game.GetBoard();
+				for (int i = 0; i < board.Count; i++) {
+					for (int j = 0; j < board[i].Count; j++) {
+						Console.Write(board[i][j] + " ");
+					}
+					Console.WriteLine();
+				}
+			};
+
+			game.OnGameOver += (sender, e) => {
+				if (e is TicTacToe.GameOverEventArgs) {
+					TicTacToe.GameOverEventArgs args = e as TicTacToe.GameOverEventArgs;
+					TicTacToe.PlayerToken winner = args.winner;
+					Console.WriteLine("Game over! Winner: " + winner);
+				}
+			};
+
+			while (!game.GameOver) {
+				// Get player input
+				Console.WriteLine($"Enter row and column (0-2) for player: {game.GetCurrPlayer()}");
+				string[] input = Console.ReadLine().Split();
+				int row = Convert.ToInt32(input[0]);
+				int col = Convert.ToInt32(input[1]);
+				game.MakeMove(row, col);
+			}
 		}
 	}
 }
 
-// class InOut {
-// 	// public static int ReadInt() {
-// 	// 	return Convert.ToInt32(Console.ReadLine());
-// 	// }
+class InOut {
+	// public static int ReadInt() {
+	// 	return Convert.ToInt32(Console.ReadLine());
+	// }
 
-// 	// public static int[] ReadIntArray(int n) {
-// 	// 	int[] arr = new int[n];
-// 	// 	string elements = Console.ReadLine();
-// 	// 	//elements = elements;
-// 	// 	arr = Array.ConvertAll(elements.Split(), int.Parse);
-// 	// 	return arr;
-// 	// }
+	// public static int[] ReadIntArray(int n) {
+	// 	int[] arr = new int[n];
+	// 	string elements = Console.ReadLine();
+	// 	//elements = elements;
+	// 	arr = Array.ConvertAll(elements.Split(), int.Parse);
+	// 	return arr;
+	// }
 
-// 	// public static void PrintArr(int[] arr) {
-// 	// 	string ret = "";
-// 	// 	foreach (int a in arr) {
-// 	// 		ret += a;
-// 	// 	}
-// 	// 	Console.WriteLine(ret);
-// 	// }
+	// public static void PrintArr(int[] arr) {
+	// 	string ret = "";
+	// 	foreach (int a in arr) {
+	// 		ret += a;
+	// 	}
+	// 	Console.WriteLine(ret);
+	// }
 
-// }
+}
--- ConsoleApp/TicTacToe/TicTacToe.cs
+++ ConsoleApp/TicTacToe/TicTacToe.cs
@@ -1,9 +1,133 @@
 
+using System.Collections;
+
 namespace Game {
 	public class TicTacToe {
-		public void Initialize() {
-			Console.WriteLine("Welcome to Tic Tac Toe!");
+		public enum PlayerToken {
+			None = 0,
+			X = 1,
+			O = 2,
 		}
+		private List<List<PlayerToken>> board;
+		private int currentPlayer = 0;
+		private bool gameOver = false;
+		public bool GameOver { get { return gameOver; } }
+		public event EventHandler OnMoveMade = delegate { };
+		public event EventHandler OnGameOver = delegate { };
+		public class MoveEventArgs : EventArgs {
+			public PlayerToken player;
+			public int row;
+			public int col;
+		}
+
+		public class GameOverEventArgs : EventArgs {
+			public PlayerToken winner;
+		}
+
+		public TicTacToe() {
+			int gridSize = 3;
+			// Constructor
+			board = new List<List<PlayerToken>>();
+			for (int i = 0; i < gridSize; i++) {
+				board.Add(new List<PlayerToken>());
+				for (int j = 0; j < gridSize; j++) {
+					board[i].Add(PlayerToken.None);
+				}
+			}
+
+			currentPlayer = (int)PlayerToken.X;
+		}
+
+		private bool moveMade = false;
+		public IEnumerator GameLoop() {
+			gameOver = false;
+			while (!IsBoardFull() && !CheckWin()) {
+				moveMade = false;
+
+				while (!moveMade) {
+					// Wait for player input
+					yield return null;
+				}
+			}
+
+			// Game over
+			gameOver = true;
+			PlayerToken winner = GetWinner();
+			OnGameOver(this, new GameOverEventArgs { winner = winner });
+		}
+
+		public void Update() {
+			if (!gameOver) {
+				GameLoop().MoveNext();
+			}
+		}
+
+		public List<List<PlayerToken>> GetBoard() {
+			return board;
+		}
+
+		private bool IsBoardFull() {
+			foreach (List<PlayerToken> row in board) {
+				foreach (PlayerToken cell in row) {
+					if (cell == PlayerToken.None) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+		public bool MakeMove(int row, int col) {
+			if (row < 0 || row >= board.Count || col < 0 || col >= board[0].Count || board[row][col] != PlayerToken.None || IsBoardFull()) {
+				return false; // Invalid move
+			}
+
+			board[row][col] = (PlayerToken)currentPlayer;
+			OnMoveMade(this, new MoveEventArgs { player = GetCurrPlayer(), row = row, col = col });
+			moveMade = true;
+			Update();
+
+			currentPlayer = currentPlayer == (int)PlayerToken.X ? (int)PlayerToken.O : (int)PlayerToken.X; // Switch player
+			return true;
+		}
+
+		private bool CheckWin() {
+			// Check rows
+			for (int i = 0; i < board.Count; i++) {
+				if (board[i][0] != PlayerToken.None && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+					return true;
+				}
+			}
+
+			// Check columns
+			for (int i = 0; i < board[0].Count; i++) {
+				if (board[0][i] != PlayerToken.None && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+					return true;
+				}
+			}
+
+			// Check diagonals
+			if (board[0][0] != PlayerToken.None && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+				return true;
+			}
+			if (board[0][2] != PlayerToken.None && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+				return true;
+			}
+
+			return false;
+		}
+
+		public PlayerToken GetCurrPlayer() {
+			return (PlayerToken)currentPlayer;
+		}
+
+		public PlayerToken GetWinner() {
+			if (CheckWin()) {
+				return (PlayerToken)currentPlayer;
+			}
+			return PlayerToken.None;
+		}
+
 	}
 
 }
